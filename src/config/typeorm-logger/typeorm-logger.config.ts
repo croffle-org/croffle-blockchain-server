27d5fdaa2bcc1ce04e58bfb5afcc2a15f55b2fb0
ash991213 <ads1213@nate.com>
2024-01-04 14:23:56 +0900

    typeorm logger 버그 수정

--- src/config/typeorm-logger/typeorm-logger.config.ts
+++ src/config/typeorm-logger/typeorm-logger.config.ts
@@ -3,6 +3,20 @@ import { QueryRunner, LogLevel, LogMessage, LogMessageType, LoggerOptions } from
 
 import { AbstractLogger } from 'typeorm';
 
+/**
+ * @dev
+ * * TypeORM의 로깅을 위한 커스텀 로거 구성
+ * @description
+ * * 1. LogLevel: 로그의 심각도 수준을 정의
+ * * 2. LogMessage: 로그 메시지의 구조를 정의
+ * * 3. LogMessageType: 가능한 로그 메시지 유형을 정의
+ * * 4. LoggerOptions: TypeORM 로거 구성에 사용되는 옵션을 정의
+ * * 5. AbstractLogger: TypeORM에 의해 사용될 로거의 추상 클래스를 제공
+ * * 6. prepareLogMessages: 로그 메시지를 준비하는 메소드로 다음과 같은 기능 제공
+ * * - SQL 하이라이팅
+ * * - SQL 사용된 파라미터 확인
+ * * - SQL 접두사에 : 추가
+ */
 export class TypeOrmLoggerConfig extends AbstractLogger {
     private readonly logger: Logger = new Logger('TypeormLogger');
 
@@ -22,22 +36,29 @@ export class TypeOrmLoggerConfig extends AbstractLogger {
 
     private getLogFunction(level: LogLevel, messageType?: LogMessageType): (...args: any[]) => void {
         const effectiveLevel = messageType ?? level;
-        switch (effectiveLevel) {
-            case 'log':
-            case 'schema-build':
-            case 'migration':
-                return this.logger.log;
-            case 'info':
-            case 'query':
-                return this.logger.verbose;
-            case 'warn':
-            case 'query-slow':
-                return this.logger.warn;
-            case 'error':
-            case 'query-error':
-                return this.logger.error;
-            default:
-                return this.logger.log;
-        }
+        const logger = this.logger;
+        return (message: any) => {
+            switch (effectiveLevel) {
+                case 'log':
+                case 'schema-build':
+                case 'migration':
+                    logger.log(message);
+                    break;
+                case 'info':
+                case 'query':
+                    logger.verbose(message);
+                    break;
+                case 'warn':
+                case 'query-slow':
+                    logger.warn(message);
+                    break;
+                case 'error':
+                case 'query-error':
+                    logger.error(message);
+                    break;
+                default:
+                    logger.log(message);
+            }
+        };
     }
 }
